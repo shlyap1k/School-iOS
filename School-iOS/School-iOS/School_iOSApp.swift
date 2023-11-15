2a2e78986370f65ab27a062b3007adf98e0c5886
Alexander Igonin <osninogi@gmail.com>
2023-11-15 08:58:51 +0300

    create project

---
+++ School-iOS/School-iOS/School_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  School_iOSApp.swift
+//  School-iOS
+//
+//  Created by Shlyap1k on 15.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct School_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
