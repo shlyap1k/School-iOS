479e956f1bea75fd3959abffbd5b365229003118
Alexander Igonin <osninogi@gmail.com>
2023-11-30 14:43:04 +0300

    extension color moved to badges

--- School-iOS/Classes/UI/ProductItem/ProductItem.swift
+++ School-iOS/Classes/UI/ProductItem/ProductItem.swift
@@ -1,32 +1,32 @@
 //
-//  ProductItem.swift
-//  School-iOS
-//
-//  Created by Shlyap1k on 29.11.2023.
+// HH School
+// Created by Shlyap1k.
 //
 
 import SwiftUI
 
+// MARK: - ProductItem
+
 struct ProductItem: View {
-    @Binding var model: Product
-    
+    var model: Product
+
     var body: some View {
-        HStack(alignment: .top) {
+        HStack(alignment: .top, spacing: 0) {
             AsyncImage(url: URL(string: model.preview)) { image in
                 image.resizable()
             } placeholder: {
                 LoaderView()
             }
             .frame(width: 116, height: 116)
-            .padding(.trailing, 16)
-            VStack(alignment: .leading) {
+            .padding(.trailing, 18)
+            VStack(alignment: .leading, spacing: 0) {
                 Text(model.title)
                     .lineLimit(3)
                     .applyStyle(.bold16)
                     .frame(width: 200, alignment: .leading)
-                    .padding(.bottom, 16)
+                    .padding(.bottom, 26)
                     .padding(.trailing, 2)
-                Badges(badges: $model.badge)
+                Badges(badges: model.badge)
             }
         }
         .padding([.leading, .trailing], 16)
@@ -36,7 +36,8 @@ struct ProductItem: View {
 
 #Preview {
     ProductItem(
-        model: .constant(Product(
+        model:
+        Product(
             id: "aboba",
             title: "Men's Nike J.J. Watt Black Arizona Cardinals Legend Jersey",
             department: "Джерси",
@@ -44,17 +45,17 @@ struct ProductItem: View {
             badge: [
                 Badge(value: "Хит сезона", color: "#3C72BF"),
                 Badge(value: "Hot", color: "#BF3C3C"),
-                Badge(value: "Скидка", color: "#3CBF49")
+                Badge(value: "Скидка", color: "#3CBF49"),
             ],
             preview: "https://fanatics.frgimages.com/FFImage/thumb.aspx?i=/productimages/_3533000/ff_3533150-d9254664c08370f8572c_full.jpg&w=340",
             images: [
-                "https://fanatics.frgimages.com/FFImage/thumb.aspx?i=/productimages/_3533000/ff_3533150-d9254664c08370f8572c_full.jpg&w=340"
+                "https://fanatics.frgimages.com/FFImage/thumb.aspx?i=/productimages/_3533000/ff_3533150-d9254664c08370f8572c_full.jpg&w=340",
             ],
             sizes: [
                 Size(value: "M", isAvailable: true),
                 Size(value: "L", isAvailable: true),
                 Size(value: "XL", isAvailable: true),
-                Size(value: "XS", isAvailable: false)
+                Size(value: "XS", isAvailable: false),
             ],
             description: "description: We want fans to celebrate their fandom by customizing and personalizing certain products. For these customizable products, including jerseys, we invite customers to tell us how they would like their preferred name or other text to appear by typing that text into the field indicated. However, just because a customer is able to type proposed customization text into the field and is able to complete the order through the website, not all proposed customization text will be accepted. Please note, Fanatics may reject and cancel any customization order for any reason, including for messages that are deemed offensive or inappropriate. Take your Arizona Cardinals fandom to the next level when you grab this Custom Game Jersey. This Nike gear is perfect for an avid Arizona Cardinals fan like you who never misses a moment of the action. The crisp graphics will put your unwavering pride on full display and ensure everyone knows who you're rooting for at the next game!",
             details: [
@@ -66,28 +67,10 @@ struct ProductItem: View {
                 "The classic fit and feel of the Pegasus is back—with a wider toe box to provide extra room. Seaming on the upper provides a better shape and fit, delivering a fresh take on an icon.",
                 "Officially licensed",
                 "Imported",
-                "Brand: Nike"
+                "Brand: Nike",
             ]
-            
+
 //                id: "061f02a0-8d12-4828-ab33-6b319a367e66"
         )
-        )
     )
-}
-
-extension Color {
-    init(hex: String) {
-        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
-        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
-
-        var rgb: UInt64 = 0
-
-        Scanner(string: hexSanitized).scanHexInt64(&rgb)
-
-        let red = Double((rgb & 0xFF0000) >> 16) / 255.0
-        let green = Double((rgb & 0x00FF00) >> 8) / 255.0
-        let blue = Double(rgb & 0x0000FF) / 255.0
-
-        self.init(red: red, green: green, blue: blue)
-    }
 }
