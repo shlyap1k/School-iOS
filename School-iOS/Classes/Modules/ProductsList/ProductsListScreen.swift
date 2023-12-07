78613be31e7bcf148d9827b1cbb4a7374dc41987
Alexander Igonin <osninogi@gmail.com>
2023-12-07 11:25:08 +0300

    delete force unwrapping

--- School-iOS/Classes/Modules/ProductsList/ProductsListScreen.swift
+++ School-iOS/Classes/Modules/ProductsList/ProductsListScreen.swift
@@ -42,7 +42,9 @@ struct ProductsListScreen: View {
         .navigationDestination(for: ProductsListRoutes.self, destination: { route in
             switch route {
             case let .product(product):
-                Text("get rid of force unwrapping in your project\n" + product!.title)
+                if let product = product {
+                    Text(product.title)
+                }
             }
         })
     }
