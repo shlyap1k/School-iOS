e582badc057c32071ef2cd911dce9bbfadb3c5f1
Alexander Igonin <osninogi@gmail.com>
2023-12-07 11:25:08 +0300

    delete force unwrapping

--- School-iOS/Classes/Modules/ProductsList/ProductsListScreen.swift
+++ School-iOS/Classes/Modules/ProductsList/ProductsListScreen.swift
@@ -10,44 +10,39 @@ struct ProductsListScreen: View {
 
     var body: some View {
         VStack {
-            if viewModel.isLoading, viewModel.page == 1 {
-                LoaderView()
-            } else {
-                ScrollView {
-                    LazyVStack {
-                        ForEach(viewModel.products, id: \.self) { product in
-                            NavigationLink(value: ProductsListRoutes.product(product)) {
-                                ProductItem(model: product)
-                                    .overlay(alignment: .bottom) {
-                                        if product.id != viewModel.products.last?.id {
-                                            Divider()
-                                                .frame(width: 343)
-                                        }
+            ScrollView {
+                LazyVStack {
+                    ForEach(viewModel.products, id: \.self) { product in
+                        NavigationLink(value: ProductsListRoutes.product(product)) {
+                            ProductItem(model: product)
+                                .overlay(alignment: .bottom) {
+                                    if product.id != viewModel.products.last?.id {
+                                        Divider()
+                                            .padding([.leading, .trailing], 16)
                                     }
-                                    .applyStyle(.bold16)
-                                    .foregroundStyle(.black)
-                                    .multilineTextAlignment(.leading)
-                            }
+                                }
+                                .applyStyle(.bold16)
+                                .foregroundStyle(.black)
+                                .multilineTextAlignment(.leading)
                         }
-                        if !viewModel.lastPageReached {
-                            NextPageLoader(viewModel: viewModel)
-                        }
+                    }
+                    if !viewModel.lastPageReached {
+                        NextPageLoader(nextPage: viewModel.nextPage)
                     }
                 }
                 .scrollIndicators(.hidden)
             }
         }
+        .isLoading($viewModel.isLoading, $viewModel.isEmpty)
         .placeholder(viewModel.placeholder)
         .navigationTitle(L10n.ProductsList.title)
         .navigationDestination(for: ProductsListRoutes.self, destination: { route in
             switch route {
             case let .product(product):
                 if let product {
-                    ProductDetailScreen(
-                        viewModel: ProductDetailVM(product: product)
-                    )
-                    .navigationTitle(product.title)
-                    .navigationBarTitleDisplayMode(.inline)
+                    ProductDetailScreen(viewModel: ProductDetailVM(product: product))
+                        .navigationTitle(product.title)
+                        .navigationBarTitleDisplayMode(.inline)
                 }
             }
         })
