69946e9b5313b95b3f26827f47951c2fda6a9339
Alexander Igonin <osninogi@gmail.com>
2023-12-12 15:22:41 +0300

    add cart to appstate

--- School-iOS/Classes/Models/AppState.swift
+++ School-iOS/Classes/Models/AppState.swift
@@ -11,25 +11,30 @@ struct AppState: Codable {
 
         @UserDefaultValue(key: session) var accessToken: String?
         @UserDefaultValue(key: profileKey) var profile: Profile?
+        @UserDefaultValue(key: cart) var cart: OrderCheckout?
 
         func read() -> AppState {
             AppState(
                 accessToken: accessToken,
-                profile: profile
+                profile: profile,
+                cart: cart
             )
         }
 
         mutating func write(_ appState: AppState) {
             accessToken = appState.accessToken
             profile = appState.profile
+            cart = appState.cart
         }
 
         // MARK: Private
 
         private static let session = "hh-school.session"
         private static let profileKey = "hh-school.profile"
+        private static let cart = "hh-school.cart"
     }
 
     var accessToken: String?
     var profile: Profile?
+    var cart: OrderCheckout?
 }
